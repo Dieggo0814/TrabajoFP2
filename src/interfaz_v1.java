a50624d90d40f34cd5676c69f442787fa4ba2aa5
Dieggo <diego.fuentes@alu.uclm.es>
2023-03-17 11:53:47 +0100

    Subida trabajo Primera parte

---
+++ src/interfaz_v1.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class interfaz_v1 {
+    private JButton añadirInformaciónDeUnButton;
+    private JButton apilarButton;
+    private JButton desapilarButton;
+    private JFormattedTextField informaciónAsociadaAUnFormattedTextField;
+    private JList list1;
+    private JPanel mainPanel;
+    private JButton verListadoDeContenedoresButton;
+
+
+}
