11f3a1edee67a5a5dc8b4e112b1f0efb7637426c
JavierCalleja <javipozorrubio888@gmail.com>
2023-03-17 12:18:54 +0100

    nombre

--- src/PaqG01/Main.java
+++ src/PaqG01/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        System.out.println("Mijael Calle Canqui");
         System.out.println("Hola");
+
+        System.out.println("Hola1");
     }
 }
