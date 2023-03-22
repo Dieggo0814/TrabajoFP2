1dbdf25c2d378c375a319329310b98c46fb3e7c4
evamaria <EvaMaria.Cuesta@alu.uclm.es>
2023-03-22 21:14:57 +0100

    Creacion de atributos de la clase contenedor, constructores, setters y getters y metodos

--- src/PaqG01/Contenedor.java
+++ src/PaqG01/Contenedor.java
@@ -84,5 +84,86 @@ public class Contenedor {
         return empresaRecibe;
     }
 
+    //metodo toString
+    public String toString(){
+        String s= "";
+        s+= "ID: " + this.ID + "\n";
+        s+= "Peso: " + this.peso + "\n";
+        s+= "Pais procedencia: " + this.paisProcedencia + "\n";
+        s+= "Prioridad: " + this.prioridad + "\n";
+        s+= "Descripcion contenido: " + this.descripcionContenido + "\n";
+        s+= "Empresa que lo envia: " + this.empresaEnvia + "\n";
+        s+= "Empresa que lo recibe: " + this.empresaRecibe + "\n";
+        if(inspeccionado){
+            s+="Inspeccionado" + "\n";
+        }else{
+            s+="No inspeccionado" + "\n";
+        }
+        return s;
+    }
+
+    //metodo apilar
+    public  void apilar(Contenedor c){
+        if(c != null){
+            int p;
+            int col;
+            p = c.getPrioridad();
+            if(p == 1 || p == 2){
+                col = p-1; //se resta 1 porque las columnas empiezan en 0
+                for(int i = hub.length-1; i >=0; i--){
+                    hub[i][col] = c;
+                    return;
+                }
+
+            }
+        } else{
+            for(int j = 2; j < hub[0].length;j++)
+                for(int i= hub.length-1; i >=0; i--){
+                    hub[i][j]=c;
+                    return;
+                }
+        }
+    }
+
+    //metodo desapilar
+    public Contenedor desapilar(int columna){
+        if(columna<0 || columna >= hub[0].length) return null;
+        for (int i = 0; i <= hub.length ; i++) {
+            if(hub[i][columna] != null){
+                Contenedor aux = hub[i][columna];
+                hub[i][columna] = null; //se ha quitado el contenedor
+                return aux;
+            }
+        }
+        return null;
+    }
+
+    //metodo para mostrar todos los datos de un contenedor a partir de su numero de ID
+    public void mostrarDatos(Contenedor c, int ID){
+        for (int i = 0; i < hub.length; i++) {
+            for (int j = 0; j < hub[i].length; j++) {
+                if(this.ID == hub[i][j].ID) {
+                    System.out.println(c.toString());
+                }
+                if(this.ID != hub[i][j].ID){
+                    System.out.println("El contenedor no esta en el hub");
+                }
+            }
+        }
+    }
+
+    //metodo para calcular la cantidad de contenedores procedentes de un determinado país
+    public void cantidadContenedores(String pais){
+        int cont=0;
+        for (int i = 0; i < hub.length; i++) {
+            for (int j = 0; j < hub[i].length; j++) {
+                if(this.paisProcedencia == hub[i][j].paisProcedencia) {
+                    cont++;
+                }
+            }
+        }
+        System.out.println("Hay " + cont + "procedentes de " + paisProcedencia);
+    }
+
 
 }
