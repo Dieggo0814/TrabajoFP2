8fb0b8c39c20c22514ae6a4df611a1dc91235a8d
evamaria <EvaMaria.Cuesta@alu.uclm.es>
2023-03-22 19:32:29 +0100

    Creacion de atributos de la clase contenedor y constructores

--- src/PaqG01/Contenedor.java
+++ src/PaqG01/Contenedor.java
@@ -12,5 +12,27 @@ public class Contenedor {
     private String empresaEnvia;
     private String empresaRecibe;
 
+    //atributos que hacen falta
+    private int columna;
+
+    //creamos un hub
+    private Contenedor[][] hub;
+
+    //constructores
+    public Contenedor(int ID){
+        if(ID < 0) ID = -ID; //si el id pasado es negativo se pone positivo
+        this.ID = ID;
+        this.prioridad = 3;
+    }
+    public Contenedor(int ID,int peso,String paisProcedencia,int prioridad,String descripcionContenido,String empresaEnvia,String empresaRecibe){
+        this.ID = ID;
+        this.peso = peso;
+        this.paisProcedencia = paisProcedencia;
+        this.prioridad = prioridad;
+        this.descripcionContenido = descripcionContenido;
+        this.empresaEnvia = empresaEnvia;
+        this.empresaRecibe = empresaRecibe;
+    }
+
 
 }
