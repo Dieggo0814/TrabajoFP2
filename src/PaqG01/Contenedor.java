c04eb29ad1e3c701d7a6e65b9604dc31149c9ece
evamaria <EvaMaria.Cuesta@alu.uclm.es>
2023-03-22 16:41:54 +0100

    Creacion de atributos de la clase contenedor

--- src/PaqG01/Contenedor.java
+++ src/PaqG01/Contenedor.java
@@ -2,5 +2,15 @@ package PaqG01;
 
 public class Contenedor {
 
+    //informacion asociada a un contenedor
+    private int ID;
+    private int peso;
+    private String paisProcedencia;
+    private boolean inspeccionado;
+    private int prioridad;
+    private String descripcionContenido;
+    private String empresaEnvia;
+    private String empresaRecibe;
+
 
 }
